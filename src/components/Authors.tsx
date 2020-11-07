d16e204d7b65f50437217d37f10444ca719c2c8d
PadmaGnanapriya <padmaisuru@gmail.com>
2020-11-07 13:25:13 +0530

    Create author curd functions in text area

--- src/components/Authors.tsx
+++ src/components/Authors.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import db from '../db/db'
+
 
 const Authors:React.FC =() =>{
 
--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -23,19 +23,42 @@ const TextArea:React.FC =()=>{
      *
      */
 
+    /* Create new author- copy existing author list and append an new author into the new copied list */
     const handleOnAuthorCreated = (newAuthor: IAuthor) => {
         const allAuthors: IAuthor[] = authors ? authors.slice() : [];
         allAuthors.push(newAuthor);
         setAuthors(allAuthors);
     };
+    /* Update new author- copy existing author list and  change the author in copied list using index */
+    const handleOnAuthorUpdated = (editedAuthor: IAuthor, index: number) => {
+        const allAuthors: IAuthor[] = authors.slice();
+        allAuthors.splice(index, 1, editedAuthor);
+        setAuthors(allAuthors);
+    };
+
+    /* Read authors - Need not function here. directly pass 'authors' is enough */
+
+    /* Delete new author- copy existing author list and delete the given author from the copied list */
+    const handleAuthorDeleted = (index: number) => {
+        const allAuthors: IAuthor[] = authors.slice();
+        allAuthors.splice(index, 1);
+        setAuthors(allAuthors);
+    };
+
 
 
 
     return(
         <React.Fragment>
             <Row>
-                <Col>Books</Col>
-                <Col><Authors authors={authors}/></Col>
+                <Col>
+                    Books
+                </Col>
+                <Col>
+                    <Authors authors={authors} onAuthorCreated={handleOnAuthorCreated}
+                              onAuthorDeleted={handleAuthorDeleted}
+                              onAuthorUpdated={handleOnAuthorUpdated}/>
+                </Col>
             </Row>
         </React.Fragment>
     );
