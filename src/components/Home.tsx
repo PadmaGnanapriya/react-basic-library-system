40114d8def7f76090dcd6df4761feba819ff6ba2
PadmaGnanapriya <padmaisuru@gmail.com>
2020-11-17 09:49:23 +0530

    fixed validation bugs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,10 @@ const Home:React.FC =() =>{
     return(
         <React.Fragment>
             <h1>Home</h1>
-            <p>React Router is a collection of navigational components that compose declaratively with your application. Whether you want to have bookmarkable URLs for your web app or a composable way to navigate in React Native, React Router works wherever React is rendering--so take your pick!</p>
+            <p>React Router is a collection of navigational components that compose declaratively with your
+                application. Whether you want to have bookmarkable URLs for your web app or a composable way
+                to navigate in React Native, React Router works wherever React is rendering--so take your pick!
+            </p>
 
         </React.Fragment>
     )
