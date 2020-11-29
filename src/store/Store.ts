f642896ec2c1f4c3a7997f1c795b21ebc0e095bd
PadmaGnanapriya <padmaisuru@gmail.com>
2020-11-29 15:42:18 +0530

    error occur

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,6 +1,21 @@
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import thunk, {ThunkMiddleware} from "redux-thunk";
+import {AuthorActions, BookActions} from "../components/types/Actions";
 import {AuthorReducer} from "./reducers/AuthorReducer";
-import { createStore} from "redux";
 import {BookReducer} from "./reducers/BookReducer";
 
-// export const Store =createStore(AuthorReducer)
-export const Store =createStore(BookReducer)
+export const rootReducer = combineReducers({
+    author: AuthorReducer,
+    book: BookReducer
+});
+
+export type AppState = ReturnType<typeof rootReducer>;
+
+export const Store =
+    createStore(rootReducer, applyMiddleware(thunk as ThunkMiddleware<AppState, AuthorActions, BookActions>));
+
+
+
+// import { createStore} from "redux";
+// import {BookReducer} from "./reducers/BookReducer";
+// export const Store =createStore(BookReducer)
