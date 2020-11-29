82a9f2512c3ced88dd031a3b4e778d525fe08d32
PadmaGnanapriya <padmaisuru@gmail.com>
2020-11-29 11:12:26 +0530

    create update author function.

--- src/components/authors/UpdateAuthor.tsx
+++ src/components/authors/UpdateAuthor.tsx
@@ -1,40 +1,47 @@
 import React, {FormEvent, useEffect, useState} from "react";
-import {IAuthor} from "../types/LibraryTypes";
+import {IAuthor, UpdatableAuthor} from "../types/LibraryTypes";
 import {Col, Button, Form} from "react-bootstrap"
+import {useDispatch} from "react-redux";
+import {updateAuthor} from "../../store/actions/AuthorActions";
 
 type UpdateAuthorProps = {
     onAuthorUpdated: (newAuthor: IAuthor, index: number) => void;
     keyIndex: number;
-    isUpdatable: (val:boolean) => void;
+    isUpdatable: (val: boolean) => void;
     author: IAuthor;
 }
 
-const UpdateAuthor:React.FC<UpdateAuthorProps> = (props) => {
-    const {keyIndex,author} = props;
+const UpdateAuthor: React.FC<UpdateAuthorProps> = (props) => {
+    const {keyIndex, author} = props;
     const [validated, setValidated] = useState(false);
-    const [authorName, setAuthorName] = useState<string|null>(author.name);
+    const [authorName, setAuthorName] = useState<string | null>(author.name);
 
     useEffect(() => {
         setAuthorName(author.name);
     }, [author])
 
-    const handleOnSubmit = (event:FormEvent) => {
+    const dispatch = useDispatch();
+    const updateAuthorDispatch = (authorData: UpdatableAuthor) => {
+        dispatch(updateAuthor(authorData));
+    };
+
+    const handleOnSubmit = (event: FormEvent) => {
         event.preventDefault();
         event.stopPropagation();
-        if(authorName !== null && authorName !== ''){
-            props.onAuthorUpdated({name: authorName},keyIndex);
+        if (authorName !== null && authorName !== '') {
+            updateAuthorDispatch({author: {name: authorName}, index: keyIndex});
             setValidated(false);
             Array.from(document.querySelectorAll("input")).forEach(
                 input => (input.value = "")
             );
             props.isUpdatable(false);
             setAuthorName(null);
-        }else {
+        } else {
             setValidated(true);
         }
     };
 
-    const onChangeAuthorField = (e:React.ChangeEvent<HTMLInputElement>)  => {
+    const onChangeAuthorField = (e: React.ChangeEvent<HTMLInputElement>) => {
         setAuthorName(e.target.value);
         setValidated(false);
     }
@@ -43,25 +50,24 @@ const UpdateAuthor:React.FC<UpdateAuthorProps> = (props) => {
         props.isUpdatable(false);
     }
 
-    return(
+    return (
         <React.Fragment>
             <div className="update-author mt-2 mb-5 pb-2 pt-5">
                 <Form.Row>
                     <Col className="text-left pl-1 mb-3">
                         <span>Update Author</span>
                     </Col>
-                    <Col  className="text-right">
+                    <Col className="text-right">
                         <i className='feather icon-x-circle text-dark text-right' onClick={onClickClose}/>
                     </Col>
                 </Form.Row>
 
                 <Form noValidate validated={validated} className="pl-5" onSubmit={handleOnSubmit}>
                     <Form.Row>
-                        <Form.Group controlId="authorSelectID"  className="form-group-dev">
+                        <Form.Group controlId="authorSelectID" className="form-group-dev">
                             <Form.Label className="text-left label-text">Name of Author</Form.Label>
-                            <Form.Control required type="text" className="form-input"
-                                          placeholder="" value={authorName ? authorName : ''}
-                                          onChange={onChangeAuthorField}/>
+                            <Form.Control required type="text" className="form-input" onChange={onChangeAuthorField}
+                                          placeholder="" value={authorName ? authorName : ''}/>
                             <Form.Control.Feedback type="invalid">Author name can not be empty!</Form.Control.Feedback>
                         </Form.Group>
                     </Form.Row>
